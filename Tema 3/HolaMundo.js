87905d8283fca35b272aa3a2759c865090ccb4d6
Pablo Calvente <calventeramirez@outlook.com>
2023-09-21 09:15:25 +0200

    Primera subida

---
+++ Tema 3/HolaMundo.js
@@ -0,0 +1,10 @@
+/*
+Instalar Node.js, ESLint, Prettier Code, Javascript Debugger (normal), Intellicode
+Live Server, Quokka.JS y Quokka Statusbar buttons
+ */
+nombre = "Pablo Calvente";
+//JS podemos definir una variable sin la palabra reservada var, let o const
+//No es una buena practica
+//var esta obsoleto y lo que se utiliza let y const para definir variables
+console.log(nombre);
+//Para llamar a quokka ctrl+Shift+P -> quokka.js: Toggle (Start/Stop)
