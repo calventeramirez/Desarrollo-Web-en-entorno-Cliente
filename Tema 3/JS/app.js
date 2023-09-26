28ebd98bcf95b22138b3b9b894c0e97c61b239c2
Pablo Calvente <calventeramirez@outlook.com>
2023-09-26 09:16:27 +0200

    Modificadas las cosas

---
+++ Tema 3/JS/app.js
@@ -0,0 +1,9 @@
+function sumar(){
+   let a = prompt("Introduzca el primer elemento:");//Esto sera un String
+   let b = prompt("Introduzca el segundo elemento:");
+
+    a = Number(a);//Transforma el String a entero
+    b = Number(b);
+
+   alert("La suma es: "+ (a+b));
+}
