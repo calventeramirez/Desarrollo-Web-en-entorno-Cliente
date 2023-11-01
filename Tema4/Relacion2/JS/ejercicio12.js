b214096a3ca6757010570623a341ad51e8607ee5
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2023-11-01 01:11:32 +0100

    Terminado el ejercicio12

--- Tema4/Relacion2/JS/ejercicio12.js
+++ Tema4/Relacion2/JS/ejercicio12.js
@@ -2,19 +2,34 @@ document.querySelectorAll("button")[0].onclick = function () {
     let elemento = document.getElementsByTagName("div")[0];
     let estilo = window.getComputedStyle(elemento);
     let valorAntiguo = estilo.getPropertyValue("left");
-    console.log(valorAntiguo);
     let valor = parseFloat(valorAntiguo) - 5;
-    console.log(valor);
-    elemento.style.setProperty("left", valor + "px");
+    if(valor >= 0){
+        elemento.style.setProperty("left", valor + "px");
+    }
 };
 
 document.querySelectorAll("button")[1].onclick = function () {
     let elemento = document.getElementsByTagName("div")[0];
     let estilo = window.getComputedStyle(elemento);
     let valorAntiguo = estilo.getPropertyValue("left");
-    console.log(valorAntiguo);
     let valor = parseFloat(valorAntiguo) + 5;
-    console.log(valor);
-    elemento.style.setProperty("left", valor + "px");
+    if(valor <= 1080){
+        elemento.style.setProperty("left", valor + "px");
+    }  
 };
 
+document.querySelector("img").onmouseenter = function () {
+    let elemento = document.getElementsByTagName("div")[0];
+    let estilo = window.getComputedStyle(elemento);
+    let valorAntiguo = estilo.getPropertyValue("left");
+    let valor = parseFloat(valorAntiguo) + 25;
+    if(valor <= 1080){
+        elemento.style.setProperty("left", valor + "px");
+    }  
+}
+
+document.querySelectorAll("button")[2].onclick = function () {
+    let elemento = document.getElementsByTagName("div")[0];
+    let estilo = window.getComputedStyle(elemento);
+    elemento.style.setProperty("left", "0px");
+}
