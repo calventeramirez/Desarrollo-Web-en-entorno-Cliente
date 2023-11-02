89fb3c6ae5197afbb7bf6651eff88ba9bf21726c
Pablo Calvente <calventeramirez@outlook.com>
2023-11-02 11:13:49 +0100

    Ejercicio 7 realizado

---
+++ Tema4/Relacion2/JS/ejercicio7.js
@@ -0,0 +1,321 @@
+let tds = document.querySelectorAll("td");
+let colores = ["red", "blue", "green", "yellow"];
+tds[0].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+    switch (this.style.backgroundColor) {
+        case "red":
+            this.style.backgroundColor = colores[1];
+            break;
+        case "blue":
+            this.style.backgroundColor = colores[2];
+            break;
+        case "green":
+            this.style.backgroundColor = colores[3];
+            break;
+        default:
+            this.style.backgroundColor = colores[0];
+            break;
+    }
+};
+
+tds[1].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[2].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[3].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[4].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[5].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[6].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[7].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[8].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[9].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[10].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[11].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[12].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[13].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[14].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
+
+tds[15].onclick = function () {
+//   if (this.style.backgroundColor == "black")
+//     this.style.backgroundColor = "white";
+//   else this.style.backgroundColor = "black";
+switch (this.style.backgroundColor) {
+    case "red":
+        this.style.backgroundColor = colores[1];
+        break;
+    case "blue":
+        this.style.backgroundColor = colores[2];
+        break;
+    case "green":
+        this.style.backgroundColor = colores[3];
+        break;
+    default:
+        this.style.backgroundColor = colores[0];
+        break;
+}
+};
