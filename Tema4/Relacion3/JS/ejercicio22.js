388770b7d3057cec486b4fc47f2724cf9b4e26ee
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2023-11-04 16:51:29 +0100

    Ejercicio 12

---
+++ Tema4/Relacion3/JS/ejercicio22.js
@@ -0,0 +1,109 @@
+let img = document.querySelectorAll("img");
+
+img[0].onclick = function () {
+  let capa = document.createElement("div");
+  capa.style.position = "fixed";
+  capa.style.top = "0";
+  capa.style.left = "0";
+  capa.style.width = "100%";
+  capa.style.height = "100%";
+  capa.style.backgroundColor = "rgba(0,0,0,0.7)";
+  capa.style.zIndex = "9";
+
+  let imgAmpliada = document.createElement("img");
+  imgAmpliada.src = "img/img1.jpg";
+  imgAmpliada.style.position = "absolute";
+  imgAmpliada.style.top = "50%";
+  imgAmpliada.style.left = "50%";
+  imgAmpliada.style.transform = "translate(-50%, -50%)";
+  imgAmpliada.style.maxWidth = "100%";
+  imgAmpliada.style.maxHeight = "100%";
+
+  capa.appendChild(imgAmpliada);
+  document.body.appendChild(capa);
+
+  capa.addEventListener("click", function () {
+    document.body.removeChild(capa);
+  });
+};
+
+img[1].onclick = function () {
+  let capa = document.createElement("div");
+  capa.style.position = "fixed";
+  capa.style.top = "0";
+  capa.style.left = "0";
+  capa.style.width = "100%";
+  capa.style.height = "100%";
+  capa.style.backgroundColor = "rgba(0,0,0,0.7)";
+  capa.style.zIndex = "9";
+
+  let imgAmpliada = document.createElement("img");
+  imgAmpliada.src = "img/img2.jpg";
+  imgAmpliada.style.position = "absolute";
+  imgAmpliada.style.top = "50%";
+  imgAmpliada.style.left = "50%";
+  imgAmpliada.style.transform = "translate(-50%, -50%)";
+  imgAmpliada.style.maxWidth = "100%";
+  imgAmpliada.style.maxHeight = "100%";
+
+  capa.appendChild(imgAmpliada);
+  document.body.appendChild(capa);
+
+  capa.addEventListener("click", function () {
+    document.body.removeChild(capa);
+  });
+};
+
+img[2].onclick = function () {
+  let capa = document.createElement("div");
+  capa.style.position = "fixed";
+  capa.style.top = "0";
+  capa.style.left = "0";
+  capa.style.width = "100%";
+  capa.style.height = "100%";
+  capa.style.backgroundColor = "rgba(0,0,0,0.7)";
+  capa.style.zIndex = "9";
+
+  let imgAmpliada = document.createElement("img");
+  imgAmpliada.src = "img/img3.jpg";
+  imgAmpliada.style.position = "absolute";
+  imgAmpliada.style.top = "50%";
+  imgAmpliada.style.left = "50%";
+  imgAmpliada.style.transform = "translate(-50%, -50%)";
+  imgAmpliada.style.maxWidth = "100%";
+  imgAmpliada.style.maxHeight = "100%";
+
+  capa.appendChild(imgAmpliada);
+  document.body.appendChild(capa);
+
+  capa.addEventListener("click", function () {
+    document.body.removeChild(capa);
+  });
+};
+
+img[3].onclick = function () {
+  let capa = document.createElement("div");
+  capa.style.position = "fixed";
+  capa.style.top = "0";
+  capa.style.left = "0";
+  capa.style.width = "100%";
+  capa.style.height = "100%";
+  capa.style.backgroundColor = "rgba(0,0,0,0.7)";
+  capa.style.zIndex = "9";
+
+  let imgAmpliada = document.createElement("img");
+  imgAmpliada.src = "img/img4.jpg";
+  imgAmpliada.style.position = "absolute";
+  imgAmpliada.style.top = "50%";
+  imgAmpliada.style.left = "50%";
+  imgAmpliada.style.transform = "translate(-50%, -50%)";
+  imgAmpliada.style.maxWidth = "100%";
+  imgAmpliada.style.maxHeight = "100%";
+
+  capa.appendChild(imgAmpliada);
+  document.body.appendChild(capa);
+
+  capa.addEventListener("click", function () {
+    document.body.removeChild(capa);
+  });
+};
