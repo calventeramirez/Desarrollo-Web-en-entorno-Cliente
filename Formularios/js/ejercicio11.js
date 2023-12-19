5785b5d00ccf04b5ca3f31bcd0ba065e821d6cf6
Pablo Calvente <calventeramirez@outlook.com>
2023-12-19 14:25:02 +0100

    Empezado ejercicio 11

---
+++ Formularios/js/ejercicio11.js
@@ -0,0 +1,10 @@
+let boton = document.getElementById("envio");
+boton.addEventListener("click", function(){
+    let dni = document.getElementsByName("dni")[0].value;
+    let nombre = document.getElementsByName("nombre")[0].value;
+    let apellidos = document.getElementsByName("apellidos")[0].value;
+    let fechaNacimiento = document.getElementsByName("fechaNacimiento")[0].value;
+    let contrasena = document.getElementsByName("pass")[0].value;
+
+    
+});
