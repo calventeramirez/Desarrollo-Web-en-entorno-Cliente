8740ceff7effc00f52412be9317e8ed61b93095a
Pablo Calvente <calventeramirez@outlook.com>
2024-02-06 14:11:46 +0100

    Mejorado el feetch

--- Promesas/fetch.js
+++ Promesas/fetch.js
@@ -3,13 +3,28 @@ document.querySelector("button").addEventListener("click", (e) =>{
     let salida = document.getElementById("salida");
     const url = "libros.json";
     
-    fetch(url)
+    const opciones ={
+        method:"GET",
+        headers:{
+            "X-RapidAPI-Key": "",
+            "X-RapidAPI-Host":""
+        }
+    }
+
+    fetch(url, opciones)
     .then((respuestaCorrecta) => {
         //return respuestaCorrecta.text();
         return respuestaCorrecta.json();
     }).then((texto) => { //Promesa del .text() que es necesaria si o si
         //Aqui iria la funcion para mostrar el json como queramos
         salida.innerHTML = texto;
+        texto = texto.library.book;
+        let plantilla ="";
+        for(const post of texto){
+            plantilla += "<h3>"+post.title+"</h3>";
+            plantilla += "<p>"+post.body+"</p>";
+        }
+        salida.innerHTML = plantilla;
     })
     .catch((respuestaIncorrecta)=>{
         console.log("Problema con el fetch: "+respuestaIncorrecta);
