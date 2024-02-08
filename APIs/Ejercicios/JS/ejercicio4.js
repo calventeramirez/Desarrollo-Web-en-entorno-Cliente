bedb0878bd78d436de459ad719d57de66ed76a49
Pablo Calvente <calventeramirez@outlook.com>
2024-02-08 11:19:38 +0100

    Añadido formulario

--- APIs/Ejercicios/JS/ejercicio4.js
+++ APIs/Ejercicios/JS/ejercicio4.js
@@ -43,3 +43,39 @@ xhr.addEventListener("error", function(respuesta){
     console.log(respuesta);
 });
 xhr.send();
+
+boton.addEventListener("click", function(e){
+    e.preventDefault();
+    let tarea = document.getElementsByName("tarea")[0].value;
+    let completada = document.getElementsByName("completada")[0].value;
+    // console.log(tarea);
+    // console.log(completada);
+    let ul = document.createElement("ul");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let text1 = document.createTextNode("Titulo: "+tarea);
+    let text2 = document.createTextNode("Estado: "+completada);
+    let boton2 = document.createElement("input");
+    boton2.setAttribute("type", "button");
+    boton2.setAttribute("value", "Cambiar de estado");
+    li1.appendChild(text1);
+    li2.appendChild(text2);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(boton2);
+    boton2.addEventListener("click", function(){
+        if(!completada){
+            text2.parentNode.removeChild(text2);
+            text2 = document.createTextNode("Estado: true");
+            li2.appendChild(text2);
+            ul.appendChild(li2);
+        }else{
+            text2.parentNode.removeChild(text2);
+            text2 = document.createTextNode("Estado: false");
+            li2.appendChild(text2);
+            ul.appendChild(li2);
+        }
+    });
+    ul.setAttribute("class", "listas");
+    lista.appendChild(ul);
+});
